04ba6396e2594792ec505e515c73dc17b85cd76f
SA-K1B <tareqshakib789@gmail.com>
2025-02-28 16:06:30 +0600

    Apply standardization in preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 df = pd.read_csv('data/raw/Salary_Data.csv')
-# print(df.head())
-print(df.info())
+print(df.head())
+# print(df.info())
 # print(df.describe())
-print(df.isnull().sum())
+# print(df.isnull().sum())
 
 # diplay unique values in each column
 # for col in df.columns:
@@ -12,6 +13,13 @@ print(df.isnull().sum())
 # drop all the null values
 df.dropna(inplace=True)
 # print(df.isnull().sum())
+# apply standard scalar transformation
 
+scaler = StandardScaler()
+df['Salary'] = scaler.fit_transform(df[['Salary']])
+df['Years of Experience'] = scaler.fit_transform(df[['Years of Experience']])
+# also age
+df['Age'] = scaler.fit_transform(df[['Age']])
+print(df.head())
 # save the dataset
 df.to_csv('data/processed/processed_data.csv', index=False)
