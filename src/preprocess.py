8a699fbd82485a17cbfdcf93979434b948c3dca2
SA-K1B <tareqshakib789@gmail.com>
2025-02-28 14:44:35 +0600

    Add data preprocessing stage

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,17 @@
 import pandas as pd
 df = pd.read_csv('data/raw/Salary_Data.csv')
-print(df.head())
-# print(df.info())
+# print(df.head())
+print(df.info())
 # print(df.describe())
 print(df.isnull().sum())
+
+# diplay unique values in each column
+# for col in df.columns:
+#     print(col, df[col].nunique())
+
+# drop all the null values
+df.dropna(inplace=True)
+print(df.isnull().sum())
+
+# save the dataset
+df.to_csv('data/processed/processed_data.csv', index=False)
